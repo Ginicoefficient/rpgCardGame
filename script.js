dd094c6c1b9aea31f1d45f34a2c5d94acab5d583
Ginicoefficient <104734969+Ginicoefficient@users.noreply.github.com>
2023-01-23 14:44:17 -0600

    addition

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const characterOptions = [
     charClass: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    dice: 6,
+    dice: [4, 3, 1],
+    diceCount: 3,
   },
   {
     elementId: "monster",
     charClass: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    dice: 4,
+    dice: [2],
+    diceCount: 1,
   },
 ];
 
@@ -20,13 +22,20 @@ function renderCharacter(characterName) {
   const character = characterOptions.find(
     (character) => character.charClass === characterName
   );
+  let diceHtml = [];
+  diceHtml = character.dice
+    .map(function (die) {
+      return `<div class="dice">${die}</div>`;
+    })
+    .join("");
+
   if (character) {
     innerHtml += `<div id="${character.elementId}">
                 <div class="character-card">
                     <h4 class="name">${character.charClass}</h4>
                     <img class="avatar" src="${character.avatar}"/>
                     <p class="health">health: <b> ${character.health} </b></p>
-                    <div class="dice-container"><div class="dice"> ${character.dice} </div></div>
+                    <div class="dice-container">${diceHtml}</div>
                 </div>    
             </div>`;
   }
@@ -35,3 +44,11 @@ function renderCharacter(characterName) {
 
 renderCharacter("Wizard");
 renderCharacter("Orc");
+
+function getDiceRollArray(diceCount) {
+  randomRoll = [];
+  for (i = 0; i <= diceCount; i++) {
+    randomRoll.push(Math.floor(Math.random() * 6 + 1));
+  }
+  return randomRoll;
+}
